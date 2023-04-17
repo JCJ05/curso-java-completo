c0cc726af7b463e1fedfca8e8faff013069dc706
JCJ05 <julitoaguerorengifo@hotmail.com>
2023-04-17 06:15:08 -0500

    Teoria Variables

--- algoritmos/src/variables/ExampleVariables.java
+++ algoritmos/src/variables/ExampleVariables.java
@@ -4,5 +4,17 @@ public class ExampleVariables {
 
     public static void main(String[] args) {
 
+        String saludar = "Hola mundo desde Java";
+        System.out.println(saludar);
+        
+        int numero = 10;
+
+        var numero3 = 17;
+
+        System.out.println("numero3 = " + numero3);
+
+        // Se dice que Java es 99% orientado a objetos porque los tipos de datos primitivos son valores y no objetos
+        // La variable var solo sirve a partir del JDK 11;
+
     }
 }
